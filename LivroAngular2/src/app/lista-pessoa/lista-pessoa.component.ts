3b4be76fef62617106aa78dacb1ef2292b2e0cdf
Éderson Fernandes <edersonluis.rf@gmail.com>
2019-12-05 14:42:56 -0300

    3.4 COMPONENTE

--- LivroAngular2/src/app/lista-pessoa/lista-pessoa.component.ts
+++ LivroAngular2/src/app/lista-pessoa/lista-pessoa.component.ts
@@ -7,9 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListaPessoaComponent implements OnInit {
 
+  pessoas: string[];
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  listar() {
   }
 
 }
