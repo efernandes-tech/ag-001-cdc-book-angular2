3c4456de86d869c162ce9e8645afc14516a4c5dc
Éderson Fernandes <edersonluis.rf@gmail.com>
2019-12-05 16:55:14 -0300

    4.9 NGSTYLE

--- LivroAngular2/src/app/app.module.ts
+++ LivroAngular2/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { NgIfComponent } from './ng-if/ng-if.component';
 import { NgSwitchCaseComponent } from './ng-switch-case/ng-switch-case.component';
 import { NgForComponent } from './ng-for/ng-for.component';
 import { NgClassComponent } from './ng-class/ng-class.component';
+import { NgStyleComponent } from './ng-style/ng-style.component';
 
 @NgModule({
   declarations: [
@@ -25,7 +26,8 @@ import { NgClassComponent } from './ng-class/ng-class.component';
     NgIfComponent,
     NgSwitchCaseComponent,
     NgForComponent,
-    NgClassComponent
+    NgClassComponent,
+    NgStyleComponent
   ],
   imports: [
     BrowserModule,
