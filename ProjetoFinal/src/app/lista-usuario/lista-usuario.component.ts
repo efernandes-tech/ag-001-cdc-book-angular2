65abb50f4575e1a2cdea976c89158f9a21b5679c
Éderson Fernandes <edersonluis.rf@gmail.com>
2019-12-17 15:41:16 -0300

    8.7 COMPONENTE DE LISTA DOS CONTATOS

--- ProjetoFinal/src/app/lista-usuario/lista-usuario.component.ts
+++ ProjetoFinal/src/app/lista-usuario/lista-usuario.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+
+import { ContatoModel } from '../modelos/contato-model';
+import { ContatosDataBaseService } from '../servicos/contatos-data-base.service';
 
 @Component({
   selector: 'app-lista-usuario',
@@ -7,9 +10,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListaUsuarioComponent implements OnInit {
 
-  constructor() { }
+  listaDeContatos: ContatoModel [] = [];
+
+  @Output() idClicado = new EventEmitter();
+
+  constructor(private dataBaseService: ContatosDataBaseService) { }
 
   ngOnInit() {
+    this.dataBaseService.enviarContato.subscribe(contatos => this.listaDeContatos = contatos);
+  }
+
+  contatoClidado(item: number) {
+    this.idClicado.emit(item);
   }
 
 }
