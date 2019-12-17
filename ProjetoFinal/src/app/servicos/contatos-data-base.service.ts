a30ee75625f1ac564120ad30f95682e4d724c59b
Éderson Fernandes <edersonluis.rf@gmail.com>
2019-12-17 13:31:43 -0300

    8.5 CLASSE DE SERVIÇO E DATA-BASE

--- ProjetoFinal/src/app/servicos/contatos-data-base.service.ts
+++ ProjetoFinal/src/app/servicos/contatos-data-base.service.ts
@@ -1,8 +1,24 @@
-import { Injectable } from '@angular/core';
+import { Injectable, EventEmitter } from '@angular/core';
+
+import { ContatoModel } from '../modelos/contato-model';
 
 @Injectable()
 export class ContatosDataBaseService {
 
+  meuContatos: ContatoModel [] = [];
+  enviarContato = new EventEmitter();
+
   constructor() { }
+
+  setContato(novoContato: ContatoModel): void {
+    this.meuContatos.push(novoContato);
+    this.enviarContato.emit(this.meuContatos);
+  }
+
+  getContato(id: number): ContatoModel {
+    let contato: ContatoModel;
+    contato = this.meuContatos[id];
+    return contato;
+  }
 
 }
