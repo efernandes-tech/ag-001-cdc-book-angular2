ca0e1ab0d7ca08a30ddab3a34db306ff28d97432
Éderson Fernandes <edersonluis.rf@gmail.com>
2019-12-16 22:01:26 -0300

    6.1 NGMODEL, VARIÁVEL DE TEMPLATE E ATRIBUTO NAME DA TAG HTML

--- LivroAngular2/src/app/formulario/formulario.component.ts
+++ LivroAngular2/src/app/formulario/formulario.component.ts
@@ -1,11 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Contatos } from './contatos';
+
 @Component({
   selector: 'app-formulario',
   templateUrl: './formulario.component.html',
   styleUrls: ['./formulario.component.css']
 })
 export class FormularioComponent implements OnInit {
+
+  contato = new Contatos('Éderson', '(99)99999-9999', 'email@email.com');
 
   constructor() { }
 
