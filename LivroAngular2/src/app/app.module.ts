1d66498a8d7448a67a53b85895c2e6e7e7fde26f
Éderson Fernandes <edersonluis.rf@gmail.com>
2019-12-16 21:39:22 -0300

    CAPÍTULO 6 FORMULÁRIOS

--- LivroAngular2/src/app/app.module.ts
+++ LivroAngular2/src/app/app.module.ts
@@ -18,6 +18,7 @@ import { NgStyleComponent } from './ng-style/ng-style.component';
 import { NgContentComponent } from './ng-content/ng-content.component';
 import { EventBindingComponent } from './event-binding/event-binding.component';
 import { InputOutputComponent } from './input-output/input-output.component';
+import { FormularioComponent } from './formulario/formulario.component';
 
 @NgModule({
   declarations: [
@@ -33,7 +34,8 @@ import { InputOutputComponent } from './input-output/input-output.component';
     NgStyleComponent,
     NgContentComponent,
     EventBindingComponent,
-    InputOutputComponent
+    InputOutputComponent,
+    FormularioComponent
   ],
   imports: [
     BrowserModule,
